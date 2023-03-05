84827a4de1e7beb6f5fc69cff318d417d6741b0b
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-06 07:00:57 +0900

    meals proceeding...

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,6 +7,7 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true);
 
   useEffect( () => {
     const fetchMeals = async () => {
@@ -27,7 +28,16 @@ const AvailableMeals = () => {
     }
   
     fetchMeals();
+    setIsLoading(false);
   },[]);
+
+  if ( isLoading ) {
+    return (
+      <section className = {classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
