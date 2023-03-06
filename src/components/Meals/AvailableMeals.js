87c93c0603febf30c0b34ccb44ad9b297fca3e26
DESKTOP-KSB7AKQ\jeff1212 <zeratool0207@naver.com>
2023-03-07 06:54:04 +0900

    orderAPP PROCEeding.........

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,14 +6,19 @@ import classes from './AvailableMeals.module.css';
 
 
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([]);
+  const [ meals, setMeals ] = useState([]);
   const [ isLoading, setIsLoading ] = useState(true);
+  const [ httpError, setHttpError ] = useState();
 
   useEffect( () => {
     const fetchMeals = async () => {
       const response = await fetch('https://meal-order-4edb9-default-rtdb.firebaseio.com/meals.json');
+
+      if ( !response.ok ) {
+        throw new Error('Something went wrong!');
+      }
+      
       const responseData = await response.json();
-  
       const loadedMeals = [];
   
       for ( const key in responseData ) {
@@ -25,16 +30,28 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     }
-  
-    fetchMeals();
-    setIsLoading(false);
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+    
   },[]);
 
   if ( isLoading ) {
     return (
       <section className = {classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if ( httpError ) {
+    return (
+      <section className = {classes.MealsError}>
+        <p>{ httpError }</p>
       </section>
     )
   }
